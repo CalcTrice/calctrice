b1a5ca5488ae4b581c215aa2e556c8a1a6203c6d
CalcTrice <mycalctrice@gmail.com>
2021-03-22 16:08:27 +0530

    Content Added

---
+++ JS/libraryVendors.js
@@ -0,0 +1,9 @@
+
+//Locomotive Scroll
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    multiplier: 5,
+});
