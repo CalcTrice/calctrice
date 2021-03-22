47f4a4beb5b73340a63394cb0ab44b55241903f0
CalcTrice <mycalctrice@gmail.com>
2021-03-22 16:07:39 +0530

    All Content Removed

--- JS/libraryVendors.js
+++
@@ -1,9 +0,0 @@
-
-//Locomotive Scroll
-import LocomotiveScroll from 'locomotive-scroll';
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    multiplier: 2,
-});
